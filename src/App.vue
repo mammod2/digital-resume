fa765bc64740b7bec105078f078e827c246bb59f
Mohammed <mammod26@gmail.com>
2022-06-26 09:58:11 +0800

    digital resume part 1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import BasicInfo from "./components/BasicInfo.vue";
+</script>
+<template>
+  <div class="bg-[url('/public/background.jpg')]">
+    <div class="h-screen flex py-12 px-10 max-w-5xl mx-auto space-x-8 p-10">
+      <section class="w-1/4 bg-opacity-20 backdrop-blur-sm p-2 text-white">
+        <BasicInfo />
+      </section>
+      <main class="w-3/4 bg-opacity-20 backdrop-blur-sm p-2">
+        <section>
+          <h1 class="text-white text-sm bg-black w-fit pl-1 pr-2 font-semibold">
+            About
+          </h1>
+          <p class="text-sm px-2 py-4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
+            molestiae explicabo modi facere deleniti voluptatum repellendus
+            earum. Quaerat dolores quasi ducimus mollitia? Quibusdam vel odit
+            eius laudantium sit corporis explicabo corrupti dolorum blanditiis
+            molestiae sequi modi dignissimos fuga deleniti, inventore dolorem
+            placeat nesciunt quam, natus facere! Suscipit fuga dolores illo
+            labore deleniti quibusdam porro necessitatibus, cupiditate
+            temporibus illum minus voluptatum est aliquid velit molestiae id.
+            Veniam labore libero natus totam. Ut tempore maiores corporis atque
+            dolorem quas nam consectetur doloremque omnis libero? Minima quam
+            iste vitae sed et veritatis vel ducimus. Architecto excepturi labore
+            voluptatem ratione perferendis eaque non ad!
+          </p>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
---
+++ src/components/BasicInfo.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="">
+    <h1 class="text-xl font-semibold">Mohammed Moatasem</h1>
+    <h2 class="text-sm font-light text-gray-200 mt-2">Frontend developer</h2>
+    <a
+      class="block mt-2 mb-6 text-xs text-grey-600 underline decoration-dashed underline-offset-2 decoration-gray-400"
+      href="mailto:mammod26@gmail.com"
+      >mammod26@gmail.com</a
+    >
+    <a
+      href="
+    "
+    ></a>
+    <ul class="pt-6 flex space-x-4">
+      <li>
+        <svg
+          class="w-4 h-4 fill-gray-400 dark:fill-gray-400 stroke-[0.1px] hover:fill-black dark:hover:fill-white transition-all"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+          ></path>
+        </svg>
+        <mdiGithub />
+      </li>
+      <li>
+        <a href="" target="blank"
+          ><svg
+            class="w-4 h-4 fill-transparent stroke-1 stroke-gray-600 hover:stroke-black hover:fill-black transition-all dark:stroke-gray-400 dark:stroke-2 dark:hover:stroke-white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+            ></path></svg
+        ></a>
+      </li>
+      <li>
+        <a href="https://github.com/mammod2" target="blank"
+          ><svg
+            class="w-4 h-4 fill-transparent stroke-1 dark:stroke-2 stroke-gray-600 hover:stroke-black hover:fill-black transition-all dark:stroke-gray-400 dark:hover:stroke-white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <title>GitHub</title>
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+            ></path></svg
+        ></a>
+      </li>
+      <li>
+        <a href="" target="blank"
+          ><svg
+            class="w-4 h-4 fill-gray-400 dark:fill-gray-300 hover:fill-black dark:hover:fill-white transition-all"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+            ></path></svg
+        ></a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script></script>
